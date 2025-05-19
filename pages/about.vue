3ebb56c37ee0783d678d1e6801b2ebb3585456dc
luisparpal <luis.parejo@mail.polimi.it>
2025-05-19 20:02:10 +0200

    First update

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,19 @@
 <template>
   <h1>Welcome to the Yoga Center</h1>
+  <h2> Our story</h2>
+  <p> Our yoga center was born from a deep passion for sharing the physical, 
+    mental, and spiritual benefits of this ancient practice. Founded in 2012, 
+    it began as a small space in Milano, where a group of yoga enthusiasts gathered 
+    to explore the harmony between body and mind. Over time, thanks to the support of our community,
+     we have grown into a sanctuary of wellness in Via Garibaldi 9, guiding students every day toward a more 
+     balanced and mindful life.</p>
+  <h2>Our inspiration</h2>
+  <p>We believe yoga is more than just poses—it's a transformative tool that helps people reconnect with themselves,
+     reduce stress, and find inner clarity. We are inspired by watching our students discover strength, flexibility, 
+     and peace through their practice. Our mission is to make yoga accessible to everyone, regardless of age, fitness level, 
+     or experience, fostering an inclusive and judgment-free space.</p>
+  <h2>Our goal</h2>
+  <h2>Join our Family</h2>
+  <p>At Om Yoga Centre, we don’t just teach yoga—we share a way of life. We invite you to discover how this practice can transform
+     your daily routine. Your journey to harmony starts here!</p>
 </template>
---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <p id="cabecera"><a href="./home">Home </a><a href="./activities"> Activities </a><a href="./teachers">Teachers</a>
+  <a href="./highlights"> About us </a><a href="./about"> About us </a><a href="./Contact"> Contact</a></p>
+  <%@include file="/WEB-INF/interno/pie.jsp"%>
+</template>
