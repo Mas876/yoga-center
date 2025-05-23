b73b770c70754eeaff1d7467382f84bd8aeacdf4
cristinaaleiva <cristinaleiva824@gmail.com>
2025-05-23 18:01:31 +0200

    Update teachers.vue

--- pages/teachers.vue
+++ pages/teachers.vue
@@ -1,13 +1,16 @@
 <template>
   <AppHeader />
   <h1>Welcome to the Yoga Center</h1>
-  
+  <section id = "teachers">
+    <h2>Meet Our Teachers</h2>
+    <TeacherList />
+  </section>
   <AppFooter />
 </template>
 
 <script setup>
 import AppHeader from '@/components/header.vue'
 import AppFooter from '@/components/footer.vue'
-/*import AppTeacher from '@/components/teachercard.vue'*/
+import TeacherList from '@/components/allprofessors.vue'
 
 </script>
