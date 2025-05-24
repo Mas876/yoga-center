f9e438434b110961ff4664a433ac04836a776dc1
cristinaaleiva <cristinaleiva824@gmail.com>
2025-05-24 11:29:45 +0200

    Update activities.vue

--- pages/activities.vue
+++ pages/activities.vue
@@ -1,11 +1,16 @@
 <template>
   <AppHeader />
   <h1>Welcome to the Yoga Center</h1>
+  <section id= "activities">
+    <h2>Meet our activities</h2>
+    <ActivitiesList />
+  </section>
   <AppFooter />
 </template>
 
 <script setup>
 import AppHeader from '@/components/header.vue'
 import AppFooter from '@/components/footer.vue'
+import ActivitiesList from '@/components/allactivities.vue'
 
 </script>
