ecb50490aa7b826e22aa8737ab5151fde6f8b757
cristinaaleiva <cristinaleiva824@gmail.com>
2025-05-24 11:29:07 +0200

    Add files via upload

---
+++ components/activitycard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class = "activity-card">
+        <img :src="photo" alt="`Photo of ${name}`" class="activity-photo"/>
+        <div class="activity-text">
+            <h2 class = "activity-name">{{ name }}</h2>
+            <h4 class = "activity-description">{{description}}</h4> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "ActivityCard",
+    props:{
+        name: {
+            type: String,
+            required: true,
+        },
+        description:{
+            type: String,
+            required: true,
+        },
+        photo:{
+            type: String,
+            required: true,
+        }
+    },
+};
+</script>
+
+<style scoped>
+    .activity-card{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #e0e0e0;
+        border: 4px solid #33691e;
+        border-radius: 8px;
+        padding: 16px;
+        max-width: 600px;
+        margin: 16px auto;
+    }
+    .activity-photo{
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+        border-radius: 50%;
+        margin-right: 16px;
+    }
+    .activity-text {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        flex: 1;
+    }
+    .activity-name{
+        font-weight: bold;
+        font-size: 16px;
+        margin-bottom:8px;
+        text-align: left;
+    }
+    .activity-description{
+        font-size: 14px;
+        color: #333;
+        text-align: left;
+        line-height: 1.5;
+    }
+</style>
---
+++ components/allactivities.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="activity-layout">
+        <ActivityCard   
+            v-for="activity in activities"
+            :key="activity.id"
+            v-bind="activity"
+        />
+    </div>
+</template>
+
+<script>
+import ActivityCard from "@/components/activitycard.vue";
+
+export default{
+    components: { ActivityCard },
+    data(){
+        return{
+            activities: [
+                {
+                    id: 1,
+                    name: "Vinyasa Flow",
+                    description: 
+                    "It is a dynamic practice that allows you to strengthen, flex, and balance your body and mind, guided by  David Lee.",
+                    photo: "/images/activity1.png",
+                },
+                {
+                    id: 2,
+                    name: "Hatha Yoga",
+                    description: 
+                    "A balanced practice that combines breath, movement, and alignment to build strength, flexibility, and mindfulness, guided by Ana Rodriguez.",
+                    photo: "/images/activity2.png",
+                },
+                {
+                    id: 3,
+                    name: "Sunrise meditation",
+                    description: 
+                    "Start your day with peace through a guided meditation that awakens your body with the first light of dawn, guided by Emily Stone.",
+                    photo: "/images/activity3.png",
+                },
+                {
+                    id: 4,
+                    name: "Weekend Yoga Retreat",
+                    description: 
+                    "Escape, relax, and recharge with a rejuvenating weekend of yoga, meditation, guided by  Ana Rodriguez and Leo Kim.",
+                    photo: "/images/activity4.png",
+                },
+                {
+                    id: 5,
+                    name: "Yoga for Beginners",
+                    description: 
+                    "Start your journey with simple and accessible poses that build strength, flexibility, and inner peace.guided by Ana Rodriguez",
+                    photo: "/images/activity5.png",
+                },
+                {
+                    id: 6,
+                    name: "Deep Relaxation Workshop",
+                    description: 
+                    "Unwind, release tension, and restore your energy with guided techniques for profound relaxation and inner peace, guided by Sara Bloom.",
+                    photo: "/images/activity6.png",
+                },
+                {
+                    id: 7,
+                    name: "Yin yoga",
+                    description: 
+                    "A slow, deep practice that enhances flexibility, relieves tension, and promotes relaxation through long-held poses, guided by  Leo Kim.",
+                    photo: "/images/activity7.png",
+                },
+                {
+                    id: 8,
+                    name: "Yoga for Stress Relief",
+                    description: "Calm your mind and body with gentle poses, breathwork, and relaxation techniques to release tension",
+                    photo: "/images/activity8.png",
+                }
+            ],
+        }
+    },
+};
+</script>
+
+<style scoped>
+    .activity-layout{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 32px;
+        padding: 40px;
+        justify-content: center;
+        background-color: #6a953c;
+    }
+</style>
