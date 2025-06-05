2b3d292b7f1c517388b38b2b9f5913ffa2a03c05
Mas876 <tbest2693@gmail.com>
2025-06-05 23:05:30 +0200

    Vercel local environemnt

---
+++ plugins/supabase.client.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default defineNuxtPlugin(nuxtApp => {
+  const config = useRuntimeConfig()
+  const supabaseUrl = config.public.supabaseUrl
+  const supabaseKey = config.public.supabaseKey
+ const supabase = createClient(supabaseUrl, supabaseKey)
+  nuxtApp.provide('supabase', supabase)
+})
--- plugins/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export default defineNuxtPlugin(nuxtApp => {
-  const supabaseUrl = 'https://whpplsmodntqdeigfbky.supabase.co'
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndocHBsc21vZG50cWRlaWdmYmt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNzE5OTgsImV4cCI6MjA2NDY0Nzk5OH0.yDBTYsSPJ4TUZvRHF_l2bX_oR3zoX25-cLuSY9EiVb0'
-  const supabase = createClient(supabaseUrl, supabaseKey)
-  nuxtApp.provide('supabase', supabase)
-})
-
