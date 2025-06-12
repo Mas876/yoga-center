5d3456a96ee55895bf7ea21a0de5bec575b928be
cristinaaleiva <cristinaleiva824@gmail.com>
2025-06-12 17:22:51 +0200

    Update highlights.vue

--- pages/highlights.vue
+++ pages/highlights.vue
@@ -10,3 +10,15 @@ import AppHeader from '@/components/header.vue'
 import AppFooter from '@/components/footer.vue'
 import highlightstext from '@/components/highlightstext.vue'
 </script>
+
+<style scoped>
+  .box{
+    padding:10px;
+    background-color: lightblue;
+  }
+  @media (min-width:768px){
+    .box{
+      padding:30px;
+    }
+  }
+</style>
