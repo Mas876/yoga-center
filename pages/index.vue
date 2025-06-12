88a7ab55a049123b797e2b8bda77b2b8c8621395
cristinaaleiva <cristinaleiva824@gmail.com>
2025-06-12 17:23:35 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,15 @@
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+  .box{
+    padding:10px;
+    background-color: lightblue;
+  }
+  @media (min-width:768px){
+    .box{
+      padding:30px;
+    }
+  }
+</style>
