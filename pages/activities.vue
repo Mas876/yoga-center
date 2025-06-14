82417fb3190f677e0052704ecb0ab1c770734a06
cristinaaleiva <cristinaleiva824@gmail.com>
2025-06-14 17:42:59 +0200

    Update activities.vue

--- pages/activities.vue
+++ pages/activities.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="box">
   <AppHeader />
   <h1>Welcome to the Yoga Center</h1>
   <section id= "activities">
@@ -15,16 +14,5 @@ import AppFooter from '@/components/footer.vue'
 import ActivitiesList from '@/components/allactivities.vue'
 
 </script>
-<style scoped>
-.box {
-  padding: 10px;
-  background-color: lightblue;
-}
 
-@media (min-width: 768px) {
-  .box {
-    padding: 30px;
-  }
-}
-</style>
 
